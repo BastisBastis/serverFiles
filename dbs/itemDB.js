65788015009fe5c096d67f8c85a920fbfe82a9ed
Sebastian Gustafsson <sebgus@gmail.com>
2020-07-18 07:08:43 +0200

    added existing server files

---
+++ dbs/itemDB.js
@@ -0,0 +1,25 @@
+export const items={
+		'stick':{
+			name:'Stick',
+			id:'stick',
+			damage:6,
+			delay:10,
+			slots:['main'],
+			spriteKey:'stick_1'
+		},
+		'sword':{
+			name:'Sword',
+			id:'sword',
+			damage:12,
+			delay:12,
+			slots:['main', 'chest'],
+			spriteKey:'sword_1'
+		},
+		't-shirt':{
+			name:'Band T-shirt',
+			id:'t-shirt',
+			armor:8,
+			slots:['chest'],
+			spriteKey:'t-shirt'
+		}
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+import Phaser from "phaser";
+import Game from "./scenes/game";
+
+console.log('hmm');
+
+const config = {
+  type: Phaser.HEADLESS,
+  parent: "phaser-example",
+  autoFocus: false,
+  width: 800,
+  height: 500,
+  physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
+        }
+    },
+  scene: [
+    Game
+  ]
+};
+
+const game = new Phaser.Game(config);
+
+window.gameLoaded();
